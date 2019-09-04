de1efbe916d65f82fd9e71a33e70d627764a97ff
Krzysztof <kpiekarz1983@gmail.com>
2019-09-04 17:51:54 +0200

    Add section-tours

---
+++ src/js/data/featuresSectionData.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    icon: 'svg-feature-world',
+    heading: 'Explore the world',
+    text:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit modi ex sit.'
+  },
+  {
+    icon: 'svg-feature-compass',
+    heading: 'Meet nature',
+    text:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum reprehenderit quasi quidem.'
+  },
+  {
+    icon: 'svg-feature-map',
+    heading: 'Find your way',
+    text:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius consequatur accusantium vitae!'
+  },
+  {
+    icon: 'svg-feature-heart',
+    heading: 'Live a healthier life',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit veritatis corrupti quas.'
+  }
+];
