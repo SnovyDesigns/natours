06ebefbf16664e2c833926b7e6f6efed6d5f9e06
Krzysztof <kpiekarz1983@gmail.com>
2019-09-05 17:41:08 +0200

    Add footer

---
+++ src/js/data/bookSectionFormData.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    inputID: 'name',
+    inputType: 'text',
+    inputPlaceholder: 'Full Name',
+    isRequired: true,
+    label: 'Full Name'
+  },
+  {
+    inputID: 'email',
+    inputType: 'email',
+    inputPlaceholder: 'Email address',
+    isRequired: true,
+    label: 'Email address'
+  }
+];
