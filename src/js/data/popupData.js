6a71d6f1ef8704ae1421864e92ccf5428ab63870
Krzysztof <kpiekarz1983@gmail.com>
2019-09-09 18:52:50 +0200

    Set up responsive version

--- src/js/data/popupData.js
+++ src/js/data/popupData.js
@@ -2,7 +2,7 @@ module.exports = {
   images: ['nat-8.jpg', 'nat-9.jpg'],
   alt: 'Tour photo',
   heading: 'Start booking now',
-  subheading: 'Important - Please read these terms before booking',
+  subheading: 'Important - Please read our terms before booking',
   text:
     'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa at pariatur hic nemo eaque quis, aut ab reprehenderit ut aperiam accusantium molestiae! Nisi facere accusantium iure, quas, veniam modi eius labore corrupti sit eveniet ea voluptates hic? Excepturi alias earum dolorum temporibus animi quia odit non impedit, cum nobis ratione numquam reiciendis maxime dolore, suscipit veritatis. Delectus velit alias nihil! Quidem magnam harum quod, nisi laboriosam sed recusandae eius nemo officia similique illum. Exercitationem asperiores, ipsum corporis explicabo nesciunt iure aliquid ut quae repellendus omnis commodi eum pariatur delectus unde, mollitia in amet facere cumque alias non! Debitis, consequatur voluptatibus!'
 };
